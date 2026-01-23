bda30fa68085893214cf46a7b1923c0e812e4177
Devan-Rejikumar <devanrejikumar007@gmail.com>
2026-01-23 20:12:05 +0530

    -First commit

---
+++ Linkedlist/linkedList.js
@@ -0,0 +1,94 @@
+class Node {
+    constructor(data){
+        this.data = data
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+    append(data){
+        const newNode = new Node(data);
+        if(!this.head){
+            this.head = newNode;
+            this.size++;
+            return;
+        }
+        let curr = this.head;
+        while(curr.next){
+            curr = curr.next;
+        }
+        curr.next = newNode;
+        this.size++;
+    }
+    prepend(data){
+        const newNode = new Node(data);
+        if(!this.head){
+            this.head = newNode;
+            this.size++;
+            return;
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.size++;
+    }
+    search(value){
+        if(this.isEmpty()) return console.log("list is empyt")
+        
+        if(this.head.value === value) return true;
+        
+        let curr = this.head;
+        
+        while(curr.next){
+            if(curr.value === value){
+                return true
+            }
+            curr = curr.next;
+        }
+        
+        return false
+    }
+    insertAtIndex(index,value){
+        if(index > this.size || index < 0){
+            return console.log('Enter a valid index')
+        }
+        if(index===0){
+            this.prepend(value)
+        }
+        else {
+            let curr = this.head;
+            for(let i = 0 ;i< index-1; i++){
+                curr=curr.next;
+            }
+            let newNode = new Node(value);
+            newNode.next = curr.next
+            curr.next = newNode
+            this.size++
+        }
+   
+        
+    }
+    print(){
+        let curr = this.head;
+        let result = '';
+        while(curr){
+            result+=curr.data+'=>';
+            curr = curr.next;
+        }
+        console.log(result+'null')
+    }
+    
+    isEmpty(){
+        return this.head === null
+    }
+}
+const list = new LinkedList();
+list.append(10);
+list.append(101);
+list.prepend(201);
+list.insertAtIndex(1,76);
+list.print();
+console.log(list.search(101))
