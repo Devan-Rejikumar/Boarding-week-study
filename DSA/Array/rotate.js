384a6fa77fbe463f7b81c9e13242f3bc77f3a512
Devan-Rejikumar <devanrejikumar007@gmail.com>
2026-02-16 10:53:50 +0530

    -rotateArray

--- DSA/Array/rotate.js
+++ DSA/Array/rotate.js
@@ -1,19 +1,13 @@
-function rotate(arr,k){
-
-let n = arr.length;
-k = k%n;
-reverse(arr,0,n-1);
-reverse(arr,0,k-1);
-reverse(arr,k,n-1)
-return arr;
+const arr = [1,2,3,4,5]
+function rotate(arr,k,n=0){
+  if(n === k) return arr
+  let popped = arr[arr.length-1];
+  for(let i = arr.length-1; i>0;i--){
+    arr[i] = arr[i-1];
+    
+  }
+  arr[0] = popped
+  return rotate(arr,k,n+1)
 }
-function reverse(arr,left,right){
-    while(left < right){
-        [arr[left],arr[right]] = [arr[right],arr[left]]
-        left++;
-        right--
-    }
-}
-let arr = [1,2,3,4,5]
-let k = 2
+let k = 3;
 console.log(rotate(arr,k))
